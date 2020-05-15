366db924437581e7191ae815ed0f87d68260ac48
alyssahellrung <alyssahellrung@gmail.com>
2020-05-15 01:39:38 -0700

    Changed the order of some things

--- Develop/server.js
+++ Develop/server.js
@@ -4,12 +4,12 @@ var app = express();
 
 var PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-require("./routes/htmlRoutes")(app);
 require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
