cdd2dee8f9970f19119c31f369fdf4f82eef16e9
alyssahellrung <alyssahellrung@gmail.com>
2020-05-15 01:06:50 -0700

    fixed duplicated package.json

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,7 @@ var express = require("express");
 
 var app = express();
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
